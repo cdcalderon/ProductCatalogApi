c5fc46ede4cc029102590fefab0565868decb1c6
carlos calderon <cdcalderon@gmail.com>
2018-07-26 07:41:05 -0500

    Add CatalogContext and configure catalogItem Entity

---
+++ src/Services/ProductCatalogApi/Data/CatalogContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductCatalogApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogApi.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CatalogBrand>(ConfigureCatalogBrand);
+            builder.Entity<CatalogType>(ConfigureCatalogType);
+            builder.Entity<CatalogItem>(ConfigureCatalogItem);
+        }
+
+        private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog");
+            builder.Property(c => c.Id)
+                .ForSqlServerUseSequenceHiLo("catalog_hilo")
+                .IsRequired(true);
+            builder.Property(c => c.Name)
+                .IsRequired(true)
+                .HasMaxLength(50);
+            builder.Property(c => c.Price)
+                .IsRequired(true);
+            builder.Property(c => c.PictureUrl)
+                .IsRequired(false);
+            builder.HasOne(c => c.CatalogBrand)
+                .WithMany()
+                .HasForeignKey(c => c.CatalogBrandId);
+            builder.HasOne(c => c.CatalogType)
+                .WithMany()
+                .HasForeignKey(c => c.CatalogTypeId);
+
+        }
+
+        private void ConfigureCatalogType(EntityTypeBuilder<CatalogType> builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConfigureCatalogBrand(EntityTypeBuilder<CatalogBrand> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
