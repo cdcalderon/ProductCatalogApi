8d7dd9d05ac266b2bea42d073a5c4ac6dff4716d
carlos calderon <cdcalderon@gmail.com>
2018-07-27 02:39:31 -0500

    Configure EF, add connection string

--- src/Services/ProductCatalogApi/Startup.cs
+++ src/Services/ProductCatalogApi/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ProductCatalogApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProductCatalogApi
 {
@@ -23,6 +25,7 @@ namespace ProductCatalogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
             services.AddMvc();
         }
 
